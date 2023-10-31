ed96bc0262920fe5f2127e26df9e162371695138
Samuel Minor <79673482+SamMi777@users.noreply.github.com>
2023-10-31 12:22:40 -0400

    3 Tests created (and failing)

---
+++ App/src/components/basicNewPost.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+test("Login button appears on the page", () => {
+    const loginButton = screen.getByRole("button", {
+        name: /Submit/i 
+    });
+    expect(loginButton).toBeInTheDocument();
+});
+
+test("Pressing Login button logs in the user", () => {
+    const loggedIn = screen.getByRole("button", {
+        name: /Submit/i 
+    });
+    loggedIn.click();
+    const loggedInButton = screen.getByRole("button", {
+        name: /Logout/i
+    });
+    expect(loggedInButton).toBeInTheDocument();
+});
+
+
+
+
