014129be98bade3a824c21bc93bc8bcc11c4c9a1
samson <samsoncournane5@gmail.com>
2023-11-26 16:34:53 -0500

    Profile popup

--- App/src/components/basicNewPost.test.tsx
+++ App/src/components/basicNewPost.test.tsx
@@ -3,14 +3,14 @@ import { render, screen } from "@testing-library/react";
 
 test("Login button appears on the page", () => {
     const loginButton = screen.getByRole("button", {
-        name: /Submit/i 
+        name: /Submit/i
     });
     expect(loginButton).toBeInTheDocument();
 });
 
 test("Pressing Login button logs in the user", () => {
     const loggedIn = screen.getByRole("button", {
-        name: /Submit/i 
+        name: /Submit/i
     });
     loggedIn.click();
     const loggedInButton = screen.getByRole("button", {
@@ -21,14 +21,7 @@ test("Pressing Login button logs in the user", () => {
 
 test("There is a create feedback button", () => {
     const feedButton = screen.getByRole("button", {
-        name: /Start new post/i 
+        name: /Start new post/i
     });
     expect(feedButton).toBeInTheDocument();
 });
-
-
-
-
-
-
-
